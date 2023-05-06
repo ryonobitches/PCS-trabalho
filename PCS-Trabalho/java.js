8d744dcbaa4df5cdefa575213d7588fa5703f7ed
ryonobitches <121812233+ryonobitches@users.noreply.github.com>
2023-05-06 10:03:08 -0300

    Add files via upload

---
+++ PCS-Trabalho/java.js
@@ -0,0 +1,12 @@
+var cred = document.getElementById("cred");
+
+
+function menu() {
+
+        document.getElementById("cred").style.display = "block";
+        return
+}
+
+function menux () {
+    document.getElementById("cred").style.display = "none";
+}
